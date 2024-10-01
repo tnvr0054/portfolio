d17a734594b96783407114efcb8a8c9608dfc04b
tnvr0054 <tanveeriqbal0054@gmail.com>
2024-10-01 09:59:01 -0700

    assignment file upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,54 @@
-import {useState,useEffect} from 'react'
-import './App.css';
-import HeroSection from './components/HeroSection';
-import Header from './components/Header';
-import About from './components/About';
-import Services from './components/Services.jsx';
-import Portfolio from './components/Portfolio.js';
-import Skills from './components/Skills.js';
-import Contact from './components/Contact.js';
-import Footer from './components/Footer.js';
+import { useState } from 'react'
+import './App.css'
+
 
 function App() {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [fileSelected, setFileSelected] = useState(null)
+  const [data, setData] = useState([])
+
+  const handleUrl = (event)=>{
+    const fileUrl = event.target.files[0]
+    setFileSelected(URL.createObjectURL(fileUrl))
+  }
+
+  const handleSubmit = ()=>{
+    const newData = {username,email,fileSelected}
+    setData([...data, newData])
+  }
+
+
   return (
     <>
-      <Header />
-      <HeroSection />
-      <About />
-      <Services />
-      <Portfolio />      
-      <Skills />      
-      <Contact />
-      <Footer />
+      <div className="form-container">
+        <div className="input-field">
+          <label>Username</label>
+          <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div className="input-field">
+          <label>Email</label>
+          <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className="input-field">
+          <label>Profile Image</label>
+          <input type='file' onChange={handleUrl} />
+        </div>
+        <button onClick={handleSubmit}>Upload Record</button>
+      </div>
+      <div className="data-container">
+        {
+          data.map(items => {
+            return <div className='card'>
+              <img src={items.fileSelected} />
+              <h1>{items.username}</h1>
+              <p>{items.email}</p>
+            </div>
+          })
+        }
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
